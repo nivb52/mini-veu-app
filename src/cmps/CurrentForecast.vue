8703f057e50a32ca5e37b18eb7529195b8a6eccb
Niv B <nivb52@gmail.com>
2019-09-24 20:14:00 +0300

    today forecast created as self cmps

---
+++ src/cmps/CurrentForecast.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <p>{{WeatherText}}</p>
+  <p>{{WeatherIcon}}</p>
+  <p>{{IsDayTime}}</p>
+  <p>{{cTemperature}} </p>
+  <p>{{fTemperature}} </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CurrentForecast",
+  props: ["weatherData"],
+  computed: {
+    cTemperature() {
+      return this.weatherData.Temperature.Metric.Value + 'c';
+    },
+    fTemperature() {
+      return this.weatherData.Temperature.Imperial.Value + 'f';
+    },
+    WeatherText() {
+       return this.weatherData.WeatherText
+    },
+    WeatherIcon() {
+       return this.weatherData.WeatherIcon
+    },
+    IsDayTime() {
+       return this.weatherData.IsDayTime ? 'day' : 'night'
+    },
+    HasPrecipitation() {
+       return this.weatherData.HasPrecipitation
+    }
+  }
+};
+</script>
+
+<style>
+</style>
