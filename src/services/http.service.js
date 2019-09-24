7a9b720150d8ca3a9fe1ac11ef71a089a11a125f
Niv B <nivb52@gmail.com>
2019-09-24 18:15:32 +0300

    created

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,27 +1,15 @@
-import router from "@/router";
+
 import axios from "axios";
 
 export default {
   get(BASE_URL, endpoint, data) {
     return ajax(BASE_URL, endpoint, "GET", null, data);
   }
-  // post(BASE_URL, endpoint, data) {
-  //     return ajax(endpoint, 'POST', data);
-  // },
-  // put(BASE_URL, endpoint, data) {
-  //     return ajax(endpoint, 'PUT', data);
-  // },
-  // delete(BASE_URL, endpoint, data) {
-  //     return ajax(endpoint, 'DELETE', data);
-  // }
 };
 
 async function ajax(
-  BASE_URL,
-  endpoint,
-  method = "get",
-  data = null,
-  params = null
+  BASE_URL,  endpoint,
+  method = "get",  data = null,  params = null
 ) {
   try {
     const res = await axios({
@@ -32,9 +20,6 @@ async function ajax(
     });
     return res.data;
   } catch (err) {
-    if (err.response.status === 401) {
-      router.push("/");
-      throw err;
-    }
+    throw err;
   }
 }
