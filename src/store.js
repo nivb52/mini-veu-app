cf6374dc3910747fe7a4499dc17ad4bf342f8486
Niv B <nivb52@gmail.com>
2019-09-27 01:43:11 +0300

    created state mang

--- src/store.js
+++ src/store.js
@@ -1,10 +1,70 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import weatherService from "@/services/weather.service.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    currentWeather: null,//weatherService.currentConditions(),
+    forcastWeather: null,//weatherService.forecasts5Days(),
+    favorites: [], // ID - NAME CURRENT - WHEATER
+    currCity: {LocalizedName:  'Tel - Aviv', Key: 215854}
+  },
+  mutations: {
+    setCurrCity(state, context) {
+      state.currCity = context.currCity
+    },
+    setCurrWeather(state, context) {
+      state.currentWeather = context.currentWeather
+    },
+    setForcastWeather(state, context) {
+      state.forcastWeather = context.setForcastWeather
+    },
+    addFavorites({favorites}, {clickedCity}) {
+      favorites.push(clickedCity)
+    },
+    removeFavorites({favorites}, {clickedCity}){
+      const cityKey = clickedCity['Key']
+      const idx = favorites.findIndex(city => city.Key === cityKey)
+      favorites.splice(idx, 1)
+    }
+  },
+  actions: {
+    async loadWeather({commit}, {pickedCity}){
+      const cityKey = pickedCity ? pickedCity['Key']  : ''    
+      commit({type: 'setCurrCity', pickedCity})
+      try {
+        const currentWeather = await weatherService.currentConditions(cityKey);
+        commit({type: 'setCurrWeather', currentWeather})
+      } catch (err) {
+          throw err
+      }
+    },
+    async loadForecast({commit}, {cityKey}){
+      try {
+        const currentWeather = await weatherService.forecasts5Days(cityKey);
+        commit({type: 'setForcastWeather', currentWeather})
+      } catch (err) {
+          throw err
+      }
+    },
+    async likeCity({commit},{clickedCity}){
+      commit({type: 'addFavorites', clickedCity})
+    },
+    async unlikeCity({commit},{clickedCity}) {
+      commit({type: 'removeFavorites', clickedCity})
+    }
+  },
+  getters: {
+    currentWeatherToShow({currentWeather}) {
+      return currentWeather
+    },
+    currentForecastToShow({forcastWeather}) {
+      return forcastWeather
+    },
+    currentCity({currCity}) {
+      return currCity
+    },
+  }
 });
