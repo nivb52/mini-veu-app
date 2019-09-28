ddf50e55110b375ba69b3886a57e419eafd30149
Niv B <nivb52@gmail.com>
2019-09-29 02:12:46 +0300

    created

---
+++ src/cmps/helpers/PageNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="page-not-found">Page not found</h2>
+</template>
+<script>
+export default {
+  name: "page-not-found"
+};
+</script>
+
+<style lang="scss">
+.page-not-found {
+  text-align: center;
+  margin-top: 24px;
+  font-weight: 500;
+  color: #888888;
+}
+</style>
