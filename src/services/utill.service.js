f2e74065b0d9f320a8e9fc566eb6aa869e25a402
Niv B <nivb52@gmail.com>
2019-09-29 02:12:12 +0300

    added getCurrLocation option

--- src/services/utill.service.js
+++ src/services/utill.service.js
@@ -1,9 +1,24 @@
-
 export default {
-    dayNameFromUTC
-  };
+  dayNameFromUTC,
+  getCurrLocation
+};
 
 function dayNameFromUTC(day) {
-    const dayIndex =  new Date(day).getUTCDay()
-    return ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"][dayIndex];
-  }
+  const dayIndex = new Date(day).getUTCDay();
+  return [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ][dayIndex];
+}
+
+function getCurrLocation() {
+  navigator.geolocation.getCurrentPosition(pos => {
+    const {latitude, longitude} = pos.coords
+    return {latitude, longitude};
+  });
+}
