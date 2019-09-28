f6b0fa3965eae85f7132e288584d878da09a9df7
Niv B <nivb52@gmail.com>
2019-09-29 00:15:32 +0300

    created default service

---
+++ src/services/default.service.js
@@ -0,0 +1,11 @@
+export default {
+    city,
+    tempUnit
+  };
+
+  function city(){
+      return {LocalizedName:  'Tel - Aviv', Key: 215854}
+  }
+  function tempUnit(){
+      return ' °F'
+  }
