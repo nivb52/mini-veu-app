46edee17f6a07f3bdfb405b1e321ce64b47d975a
Niv B <nivb52@gmail.com>
2019-09-29 02:11:38 +0300

    change store defaults and fix typo

--- src/store.js
+++ src/store.js
@@ -1,27 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import weatherService from "@/services/weather.service.js";
+import defaultService from '@/services/default.service.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentWeather: null,//weatherService.currentConditions(),
-    forcastWeather: null,//weatherService.forecasts5Days(),
-    favorites: [], // ID - NAME CURRENT - WHEATER
-    currCity: {LocalizedName:  'Tel - Aviv', Key: 215854}
+    currentWeather: [] ,
+    forecastWeather: {} ,
+    favorites: [{}], // ID - NAME CURRENT - WHEATER
+    currCity: defaultService.city()
   },
   mutations: {
     setCurrCity(state, context) {
-      state.currCity = context.currCity
+      state.currCity = context.pickedCity
     },
     setCurrWeather(state, context) {
       state.currentWeather = context.currentWeather
     },
-    setForcastWeather(state, context) {
-      state.forcastWeather = context.setForcastWeather
+    setForcastWeather(state, context) {  
+      state.forecastWeather = context.forecastWeather
     },
     addFavorites({favorites}, {clickedCity}) {
+      console.log(favorites);
       favorites.push(clickedCity)
     },
     removeFavorites({favorites}, {clickedCity}){
@@ -32,22 +34,26 @@ export default new Vuex.Store({
   },
   actions: {
     async loadWeather({commit}, {pickedCity}){
-      const cityKey = pickedCity ? pickedCity['Key']  : ''    
       commit({type: 'setCurrCity', pickedCity})
       try {
-        const currentWeather = await weatherService.currentConditions(cityKey);
+        const cityKey = pickedCity ? pickedCity['Key']  : ''    
+        const currentWeather = await weatherService.currentConditions(cityKey);        
         commit({type: 'setCurrWeather', currentWeather})
       } catch (err) {
           throw err
       }
     },
-    async loadForecast({commit}, {cityKey}){
-      try {
-        const currentWeather = await weatherService.forecasts5Days(cityKey);
-        commit({type: 'setForcastWeather', currentWeather})
+    async loadForecast({commit}, {pickedCity}){
+    const cityKey = pickedCity ? pickedCity['Key']  : ''    
+    try {
+      const forecastWeather = await weatherService.forecasts5Days(cityKey);      
+      commit({type: 'setForcastWeather', forecastWeather})
       } catch (err) {
           throw err
       }
+    },
+    async changeCurrCity({commit},{pickedCity}){
+      commit({type: 'setCurrCity', pickedCity})
     },
     async likeCity({commit},{clickedCity}){
       commit({type: 'addFavorites', clickedCity})
@@ -60,11 +66,14 @@ export default new Vuex.Store({
     currentWeatherToShow({currentWeather}) {
       return currentWeather
     },
-    currentForecastToShow({forcastWeather}) {
-      return forcastWeather
+    currentForecastToShow({forecastWeather}) {
+      return forecastWeather
     },
     currentCity({currCity}) {
       return currCity
+    },
+    myFavorites({favorites}) {
+      return favorites
     },
   }
 });
