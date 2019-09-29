21a3dbb377d38eb0232de031575d45f2ba4ffc62
Niv B <nivb52@gmail.com>
2019-09-29 06:24:26 +0300

    flex wrap by bulma: is-multiline

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -5,13 +5,16 @@
     <div>
       <div v-if="!myFavorites" class="is-capitalized subtitle centerd">
         when your add favorites you will see them here</div>
-      <div class="columns" v-else>
-        <div class="column is-4" v-for="(favorite,index) in myFavorites" :key="index">
+      <div class="columns is-multiline" v-else>
+        <div class="column 
+        is-3-desktop is-4-tablet is-full-mobile " 
+        v-for="(favorite,index) in myFavorites" :key="index">
           <CurrentWeather
             :weatherData="favorite.weatherData"
             :city="favorite.LocalizedName"
             :cityKey="favorite.Key"
             :style="{ backgroundColor: 'black' }"
+
           />
         </div>
       </div>
@@ -40,6 +43,6 @@ export default {
       console.log(this.$store.getters.myFavorites);
       return this.$store.getters.myFavorites;
     }
-  }
+  },
 };
 </script>
