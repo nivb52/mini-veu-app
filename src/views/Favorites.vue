2de4ca5b5fbd0e2a6042f994eb4cf323611ee6d7
Niv B <nivb52@gmail.com>
2019-09-29 05:08:30 +0300

    render current-weather cmp and connected to store

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    <h1>favorites page</h1>
+  <div class="container">
+    <h1 class="title is-capitalized">my favorites cities</h1>
+
+    <div>
+      <div v-if="!myFavorites" class="is-capitalized subtitle centerd">
+        when your add favorites you will see them here</div>
+      <div class="columns" v-else>
+        <div class="column is-4" v-for="(favorite,index) in myFavorites" :key="index">
+          <CurrentWeather
+            :weatherData="favorite.weatherData"
+            :city="favorite.LocalizedName"
+            :cityKey="favorite.Key"
+            :style="{ backgroundColor: 'black' }"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CurrentWeather from "@/cmps/CurrentWeather";
+import utillService from '@/services/utill.service.js'
+
 export default {
-  name: "favorites"
+  name: "favorites",
+  components: {
+    CurrentWeather
+  },
+  created () {
+    const favorites = utillService.getSession()
+     this.$store.dispatch({
+          type: "loadFavorites",
+          favorites
+        });
+  },
+  computed: {
+    myFavorites() {
+      console.log(this.$store.getters.myFavorites);
+      return this.$store.getters.myFavorites;
+    }
+  }
 };
 </script>
-
-<style></style>
