8de1473ede42bf64dbd7b425e2a73a912444677c
Niv B <nivb52@gmail.com>
2019-10-02 03:32:31 +0300

    fix favorites from error at app init

--- src/store.js
+++ src/store.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
   state: {
     currentWeather: [],
     forecastWeather: {},
-    favorites: utillService.getSession(),
+    favorites: utillService.getSession() || [],
     currCity: defaultService.city(),
     isFahrenheit: defaultService.isFahrenheit()
   },
