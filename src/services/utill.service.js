d33e9134bab72b2d78644452f57a6234a79850b4
Niv B <nivb52@gmail.com>
2019-11-28 03:46:34 +0200

    added storage utills

--- src/services/utill.service.js
+++ src/services/utill.service.js
@@ -1,7 +1,9 @@
 export default {
   dayNameFromUTC,
   setSession,
-  getSession
+  getSession,
+  storageStore,
+  storageLoad
 };
 
 function dayNameFromUTC(day) {
@@ -22,5 +24,16 @@ async function setSession(data, key = "favorites") {
 }
 function getSession(key = "favorites") {
   return JSON.parse(sessionStorage.getItem(key)) || null;
+}
+
+
+function storageStore(key, data) {
+    localStorage[key] = JSON.stringify(data)
+}
+
+function storageLoad(key) {
+  var str = localStorage[key] || "null";
+  if (str === "undefined") return false;
+  else return JSON.parse(str);
 }
 
