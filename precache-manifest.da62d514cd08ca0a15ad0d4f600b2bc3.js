636c2ea437747d80f9f4aa88da92f7da464b34d0
Niv B <nivb52@gmail.com>
2019-11-28 18:53:50 +0200

    updated as source

---
+++ precache-manifest.da62d514cd08ca0a15ad0d4f600b2bc3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b42a25c02077a02c58a480dff59e32b",
+    "url": "/weather-vue-app/bgimg/thunder.jpg"
+  },
+  {
+    "revision": "0a4bba37c221e8ed592a",
+    "url": "/weather-vue-app/css/app.19045416.css"
+  },
+  {
+    "revision": "f7ee8ea5350e04e48e64",
+    "url": "/weather-vue-app/css/chunk-1838ef70.4a468d2b.css"
+  },
+  {
+    "revision": "f7ee8ea5350e04e48e64",
+    "url": "/weather-vue-app/js/chunk-1838ef70.94e79dee.js"
+  },
+  {
+    "revision": "011e9031dd6db2a8114a",
+    "url": "/weather-vue-app/css/chunk-7d093cec.ef533f0d.css"
+  },
+  {
+    "revision": "011e9031dd6db2a8114a",
+    "url": "/weather-vue-app/js/chunk-7d093cec.873f0e61.js"
+  },
+  {
+    "revision": "4ef2ab9dca164c524196",
+    "url": "/weather-vue-app/css/chunk-vendors.c9ac70e6.css"
+  },
+  {
+    "revision": "4ef2ab9dca164c524196",
+    "url": "/weather-vue-app/js/chunk-vendors.77c2e500.js"
+  },
+  {
+    "revision": "6f5e5b425a416fe2bc22c674356eb6c0",
+    "url": "/weather-vue-app/img/my_loc-24px.6f5e5b42.svg"
+  },
+  {
+    "revision": "b1d8966b5f7c5e756c8ab2b429a291de",
+    "url": "/weather-vue-app/img/heart-multiple-outline.b1d8966b.svg"
+  },
+  {
+    "revision": "f8019d770a39af459b311847945520ed",
+    "url": "/weather-vue-app/img/heart-multiple.f8019d77.svg"
+  },
+  {
+    "revision": "e711cec413fd010972b185122cb09478",
+    "url": "/weather-vue-app/index.html"
+  },
+  {
+    "revision": "706bd34581dd47cb205c9ff4a828e7ca",
+    "url": "/weather-vue-app/img/1px.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/weather-vue-app/robots.txt"
+  },
+  {
+    "revision": "0a4bba37c221e8ed592a",
+    "url": "/weather-vue-app/js/app.9148814f.js"
+  },
+  {
+    "revision": "fe225ce0126054f3cc3709a735055a46",
+    "url": "/weather-vue-app/img/logo1.png"
+  },
+  {
+    "revision": "10c7657a5ee8168ac209ce4cb2848476",
+    "url": "/weather-vue-app/bgimg/heavy-rain-day.jpg"
+  },
+  {
+    "revision": "837bb0cfbc52e550ff35fccba8d9cc8a",
+    "url": "/weather-vue-app/bgimg/fog-night.jpg"
+  },
+  {
+    "revision": "5841c27f3a4161b5f125dff0258ac3e2",
+    "url": "/weather-vue-app/bgimg/light-rain-night.jpg"
+  },
+  {
+    "revision": "b6504ed94af74092d5674fa9ad908b49",
+    "url": "/weather-vue-app/bgimg/light-rain-day.jpg"
+  },
+  {
+    "revision": "a375c88be6bea9ddb58161ca99b6f2f7",
+    "url": "/weather-vue-app/bgimg/heavy-rain-night.jpg"
+  },
+  {
+    "revision": "e6ed6350ad22443999e4c2aac1657a32",
+    "url": "/weather-vue-app/bgimg/sunny.jpg"
+  },
+  {
+    "revision": "e459eeb5493977c27af5c42cca179989",
+    "url": "/weather-vue-app/bgimg/partly-cloudy-night.jpg"
+  },
+  {
+    "revision": "4d67ddc5eae0596bd3093afe5b14b712",
+    "url": "/weather-vue-app/bgimg/snow-light.jpg"
+  },
+  {
+    "revision": "2efd5cc037acb738ac5b19341b653de1",
+    "url": "/weather-vue-app/bgimg/ice-pellets.jpg"
+  },
+  {
+    "revision": "54ad0444e2e3b14d84e62083bc42b7db",
+    "url": "/weather-vue-app/bgimg/partly-cloudy-day.jpg"
+  },
+  {
+    "revision": "fe8c4b69be6489e29f45754c766aaeeb",
+    "url": "/weather-vue-app/bgimg/snow.jpg"
+  },
+  {
+    "revision": "475e631b8a029cf11df28c480c56cf90",
+    "url": "/weather-vue-app/bgimg/fog-day.jpg"
+  },
+  {
+    "revision": "14ca9418ac513e240063b4c6be66d69c",
+    "url": "/weather-vue-app/bgimg/clear.jpg"
+  }
+];
